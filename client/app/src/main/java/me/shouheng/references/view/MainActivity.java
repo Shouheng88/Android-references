c2bad3e99855e0ffbabfd72649381201593cb520
Wang Shouheng <shouheng2015@gmail.com>
2018-06-08 13:43:10 +0800

    use material drawer

--- client/app/src/main/java/me/shouheng/references/view/MainActivity.java
+++ client/app/src/main/java/me/shouheng/references/view/MainActivity.java
@@ -3,6 +3,18 @@ package me.shouheng.references.view;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
 import me.shouheng.references.R;
 import me.shouheng.references.base.CommonDaggerActivity;
 import me.shouheng.references.databinding.ActivityMainBinding;
@@ -18,5 +30,70 @@ public class MainActivity extends CommonDaggerActivity<ActivityMainBinding> {
     @Override
     protected void doCreateView(Bundle savedInstanceState) {
         startActivity(new Intent(this, AppIntroActivity.class));
+
+        setupDrawer(savedInstanceState);
+    }
+
+    private void setupDrawer(Bundle savedInstanceState) {
+        IProfile profile = new ProfileDrawerItem()
+                .withName("WngShhng")
+                .withEmail("shouheng2015@gmail.com")
+                .withIcon(R.drawable.ic_account)
+                .withIdentifier(100);
+        ProfileSettingDrawerItem item1 = new ProfileSettingDrawerItem()
+                .withName("Add Account")
+                .withDescription("Add new GitHub Account")
+                .withIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_add).actionBar().paddingDp(5).colorRes(R.color.material_drawer_primary_text))
+                .withIdentifier(100000);
+        ProfileSettingDrawerItem item2 = new ProfileSettingDrawerItem()
+                .withName("Manage Account")
+                .withIcon(GoogleMaterial.Icon.gmd_settings)
+                .withIdentifier(100001);
+
+        AccountHeader header = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header_bg)
+                .addProfiles(profile, item1, item2)
+                .build();
+
+        PrimaryDrawerItem drawerItem0 = new PrimaryDrawerItem()
+                .withName("Drawer item 0")
+                .withDescription("Drawer description 0")
+                .withIcon(GoogleMaterial.Icon.gmd_brightness_5)
+                .withIdentifier(0)
+                .withSelectable(true);
+        PrimaryDrawerItem drawerItem1 = new PrimaryDrawerItem()
+                .withName("Drawer item 1")
+                .withDescription("Drawer description 1")
+                .withIcon(FontAwesome.Icon.faw_home)
+                .withIdentifier(1)
+                .withSelectable(true);
+        PrimaryDrawerItem drawerItem2 = new PrimaryDrawerItem()
+                .withName("Drawer item 2")
+                .withDescription("Drawer description 2")
+                .withIcon(FontAwesome.Icon.faw_gamepad)
+                .withIdentifier(2)
+                .withSelectable(true);
+
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withHasStableIds(true)
+                .addDrawerItems(drawerItem0, drawerItem1, drawerItem2)
+                .withOnDrawerItemClickListener((view, position, drawerItem) -> {
+                    if (drawerItem == null) return false;
+                    switch ((int) drawerItem.getIdentifier()) {
+                        case 0:
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                    }
+                    return false;
+                })
+                .withAccountHeader(header)
+                .withSavedInstance(savedInstanceState)
+                .withShowDrawerOnFirstLaunch(true)
+                .build();
     }
 }
