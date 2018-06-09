49824d2ececf932e86de4b497cbd62d53b0b7564
WngShhng <shouheng2015@gmail.com>
2018-06-09 09:58:02 +0800

    add color method

--- commons/src/main/java/me/shouheng/commons/util/ColorUtils.java
+++ commons/src/main/java/me/shouheng/commons/util/ColorUtils.java
@@ -48,4 +48,11 @@ public class ColorUtils {
     public static @ColorInt int fadeColor(@ColorInt int color, @FloatRange(from = 0, to = 1) float rate) {
         return (color & 0x00ffffff) | ((0xff - (int)(0xff * rate)) << 24);
     }
+
+    public static int getBlackWhiteColor(int color) {
+        double darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255;
+        if (darkness >= 0.5) {
+            return Color.WHITE;
+        } else return Color.BLACK;
+    }
 }
