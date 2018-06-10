18fc94c3094ae9cfbd76608373efee1d27c77369
WngShhng <shouheng2015@gmail.com>
2018-06-10 20:41:28 +0800

    use fabric

--- client/app/src/main/java/me/shouheng/references/MyApplication.java
+++ client/app/src/main/java/me/shouheng/references/MyApplication.java
@@ -3,6 +3,7 @@ package me.shouheng.references;
 import android.app.Activity;
 import android.support.v4.app.Fragment;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
 
 import javax.inject.Inject;
@@ -11,6 +12,7 @@ import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
 import dagger.android.support.HasSupportFragmentInjector;
+import io.fabric.sdk.android.Fabric;
 import me.shouheng.commons.BaseApplication;
 import me.shouheng.references.di.DaggerAppComponent;
 
@@ -33,6 +35,7 @@ public class MyApplication extends BaseApplication implements HasActivityInjecto
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         application = this;
 
