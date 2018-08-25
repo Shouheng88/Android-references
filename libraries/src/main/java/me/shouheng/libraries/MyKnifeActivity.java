b2b444a03c4461d3e9a5dd4da9a5553049dc1d3e
WngShhng <shouheng2015@gmail.com>
2018-08-25 14:13:05 +0800

    change knife gradle configuration

--- libraries/src/main/java/me/shouheng/libraries/MyKnifeActivity.java
+++ libraries/src/main/java/me/shouheng/libraries/MyKnifeActivity.java
@@ -8,8 +8,6 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import me.shouheng.commons.config.BaseConstants;
 import me.shouheng.commons.tools.ToastUtils;
 import me.shouheng.commons.view.activity.CommonActivity;
-import me.shouheng.knife.annotation.BindView;
-import me.shouheng.knife.annotation.OnClick;
 import me.shouheng.knife.api.MyKnife;
 import me.shouheng.libraries.databinding.ActivityMyKnifeBinding;
 
@@ -18,10 +16,10 @@ import me.shouheng.libraries.databinding.ActivityMyKnifeBinding;
 @Route(path = BaseConstants.LIBRARY_MY_KNIFE)
 public class MyKnifeActivity extends CommonActivity<ActivityMyKnifeBinding> {
 
-    @BindView(id = R.id.tv)
+//    @BindView(id = R.id.tv)
     public TextView textView;
 
-    @OnClick(ids = {R.id.btn})
+//    @OnClick(ids = {R.id.btn})
     public void OnClick() {
         ToastUtils.makeToast("OnClick");
     }
