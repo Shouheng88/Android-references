bcf3b1b76b6e01a484f25832e0f03cd1f7b78453
Wang Shouheng <shouheng2015@gmail.com>
2018-08-29 11:09:16 +0800

    add target view test

--- animations/src/main/java/me/shouheng/animations/MainActivity.java
+++ animations/src/main/java/me/shouheng/animations/MainActivity.java
@@ -1,6 +1,7 @@
 package me.shouheng.animations;
 
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.ImageView;
@@ -11,9 +12,12 @@ import android.widget.Toast;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.balysv.materialripple.MaterialRippleLayout;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
 
 import me.shouheng.animations.databinding.ActivityMainBinding;
 import me.shouheng.commons.config.BaseConstants;
+import me.shouheng.commons.tools.PalmUtils;
 import me.shouheng.commons.tools.ToastUtils;
 import me.shouheng.commons.view.activity.CommonActivity;
 
@@ -27,23 +31,13 @@ public class MainActivity extends CommonActivity<ActivityMainBinding> {
 
     @Override
     protected void doCreateView(Bundle savedInstanceState) {
-        getBinding().btnReveal.setOnClickListener(v ->
-                ARouter.getInstance()
-                        .build(BaseConstants.ANIMATIONS_CIRCLE_REVEAL)
-                        .navigation());
+        getBinding().btnReveal.setOnClickListener(v -> ARouter.getInstance().build(BaseConstants.ANIMATIONS_CIRCLE_REVEAL).navigation());
+
         getBinding().btnToast.setOnClickListener(v -> showToastWithIcon());
-        MaterialRippleLayout.on(getBinding().tvRipple)
-                .rippleAlpha(0.2f)
-                .rippleHover(true)
-                .rippleColor(Color.BLUE)
-                .create();
-        MaterialRippleLayout.on(getBinding().tvRipple2)
-                .rippleColor(Color.parseColor("#FF0000"))
-                .rippleAlpha(0.2f)
-                .rippleHover(true)
-                .create();
-        getBinding().tvRipple.setOnClickListener(v -> ToastUtils.makeToast("Ripple1"));
-        getBinding().tvRipple2.setOnClickListener(v -> ToastUtils.makeToast("Ripple2"));
+
+        configMaterialRipple();
+
+        configTargetView();
     }
 
     private void showToastWithIcon() {
@@ -59,5 +53,51 @@ public class MainActivity extends CommonActivity<ActivityMainBinding> {
         ly.addView(iv);
         toast.setView(ly);
         toast.show();
+    }
+
+    private void configMaterialRipple() {
+        MaterialRippleLayout.on(getBinding().tvRipple)
+                .rippleAlpha(0.2f)
+                .rippleHover(true)
+                .rippleColor(Color.BLUE)
+                .create();
+        MaterialRippleLayout.on(getBinding().tvRipple2)
+                .rippleColor(Color.parseColor("#FF0000"))
+                .rippleAlpha(0.2f)
+                .rippleHover(true)
+                .create();
+        getBinding().tvRipple.setOnClickListener(v -> ToastUtils.makeToast("Ripple1"));
+        getBinding().tvRipple2.setOnClickListener(v -> ToastUtils.makeToast("Ripple2"));
+    }
+
+    private void configTargetView() {
+        Rect rect = new Rect(100, 100, 100, 100);
+        new TapTargetSequence(this)
+                .targets(
+                        TapTarget.forView(getBinding().btnReveal, "Gonna"),
+                        TapTarget.forView(getBinding().btnToast, "You", "Up")
+                                .dimColorInt(Color.RED)
+                                .outerCircleColorInt(Color.GREEN)
+                                .targetCircleColorInt(Color.BLUE)
+                                .textColorInt(Color.WHITE),
+                        TapTarget.forBounds(rect, "Down", ":^)")
+                                .cancelable(false)
+                                .icon(PalmUtils.getDrawableCompact(R.drawable.ic_account_circle_black_24dp)))
+                .listener(new TapTargetSequence.Listener() {
+                    @Override
+                    public void onSequenceFinish() {
+                        ToastUtils.makeToast("onSequenceFinish");
+                    }
+
+                    @Override
+                    public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+                        ToastUtils.makeToast("onSequenceStep");
+                    }
+
+                    @Override
+                    public void onSequenceCanceled(TapTarget lastTarget) {
+                        ToastUtils.makeToast("onSequenceCanceled");
+                    }
+                }).start();
     }
 }
