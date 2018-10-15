a2b62c20bc4f17513f154afba41114e0dcfdb93d
WngShhng <shouheng2015@gmail.com>
2018-10-15 20:57:25 +0800

    add custom view

---
+++ layout/src/main/java/me/shouheng/layout/view/custom/CustomView.java
@@ -0,0 +1,41 @@
+package me.shouheng.layout.view.custom;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author shouh
+ * @version $Id: CustomView, v 0.1 2018/10/13 11:31 shouh Exp$
+ */
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
