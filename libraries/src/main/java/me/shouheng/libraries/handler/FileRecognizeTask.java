ab026a1ebebbb3158236f87fff6f83ba16d67951
WngShhng <shouheng2015@gmail.com>
2018-11-05 20:59:31 +0800

    add handle demo code

---
+++ libraries/src/main/java/me/shouheng/libraries/handler/FileRecognizeTask.java
@@ -0,0 +1,31 @@
+package me.shouheng.libraries.handler;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import me.shouheng.commons.tools.LogUtils;
+
+/**
+ * @author shouh
+ * @version $Id: FileRecognizeTask, v 0.1 2018/11/4 23:40 shouh Exp$
+ */
+public class FileRecognizeTask extends IntentService {
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, FileRecognizeTask.class);
+        context.startService(intent);
+    }
+
+    public FileRecognizeTask() {
+        super("FileRecognizeTask");
+    }
+
+
+    @Override
+    protected void onHandleIntent(@androidx.annotation.Nullable @Nullable Intent intent) {
+        // do something
+        LogUtils.d("=========================================");
+    }
+}
--- libraries/src/main/java/me/shouheng/libraries/handler/HandlerActivity.java
+++ libraries/src/main/java/me/shouheng/libraries/handler/HandlerActivity.java
@@ -1,7 +1,8 @@
-package me.shouheng.libraries;
+package me.shouheng.libraries.handler;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 
@@ -10,6 +11,7 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import me.shouheng.commons.config.BaseConstants;
 import me.shouheng.commons.tools.LogUtils;
 import me.shouheng.commons.view.activity.CommonActivity;
+import me.shouheng.libraries.R;
 import me.shouheng.libraries.databinding.ActivityHandlerBinding;
 
 /**
@@ -18,7 +20,18 @@ import me.shouheng.libraries.databinding.ActivityHandlerBinding;
 @Route(path = BaseConstants.LIBRARY_HANDLER)
 public class HandlerActivity extends CommonActivity<ActivityHandlerBinding> {
 
-    private static Handler handler = new Handler();
+    private final static int SAY_HELLO = 1;
+
+    private static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SAY_HELLO:
+                    LogUtils.d("Hello!");
+                    break;
+            }
+        }
+    };
 
     @Override
     protected int getLayoutResId() {
@@ -35,11 +48,34 @@ public class HandlerActivity extends CommonActivity<ActivityHandlerBinding> {
             try {
                 // new Handler(); // 会因为该线程中没有对应的 Looper 而发生异常
                 Thread.sleep(2000);
-                new Handler(Looper.getMainLooper()).post(() -> getBinding().tv.setText("任务执行完毕，主线程更新UI"));
+                new Handler(Looper.getMainLooper())
+                        .post(() -> getBinding().tv.setText("任务执行完毕，主线程更新UI"));
 //                handler.post(() -> getBinding().tv.setText("任务执行完毕，主线程更新UI"));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
+
+        new Thread(() -> {
+            LogUtils.d("+++++++++" + Thread.currentThread());
+            Looper.prepare();
+            new Handler().post(() -> LogUtils.d("+++++++++" + Thread.currentThread()));
+            Looper.loop();
+        }).start();
+
+        Message message = Message.obtain(handler);
+        message.what = SAY_HELLO;
+        message.sendToTarget();
+        // or below
+//        Message message = Message.obtain();
+//        message.what = SAY_HELLO;
+//        handler.sendMessage(message);
+
+        // HandlerThread demo
+        HandlerThread handlerThread = new HandlerThread("MyHandlerThread");
+        handlerThread.start();
+
+        // IntentService demo
+        FileRecognizeTask.start(this);
     }
 }
