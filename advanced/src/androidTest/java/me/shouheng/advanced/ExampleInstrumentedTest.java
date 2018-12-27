2ef1bbec3588a3a689590fe7231c908c670a1c7e
Wang Shouheng <shouheng2015@gmail.com>
2018-12-27 19:24:08 +0800

    unit test

--- advanced/src/androidTest/java/me/shouheng/advanced/ExampleInstrumentedTest.java
+++ advanced/src/androidTest/java/me/shouheng/advanced/ExampleInstrumentedTest.java
@@ -1,13 +1,17 @@
 package me.shouheng.advanced;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +20,16 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("me.shouheng.advanced.test", appContext.getPackageName());
+    }
+
+    public void testClick(int id) {
+        onView(withId(id)).perform(click());
     }
 }
